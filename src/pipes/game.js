4d3a689c6d1a572c7eb451c11033d2c83bb75d1b
jenin-kii <rosaxxhybrida@gmail.com>
2023-08-16 08:31:09 +0000

    feat: connect aws s3 for image uploading

--- src/pipes/game.js
+++ src/pipes/game.js
@@ -2,6 +2,23 @@
 const fileUpload = require('express-fileupload');
 const Database = require('../../database/connection.js');
 const db = new Database();
+const aws = require('aws-sdk');
+const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ACCESS,
+    secretAccessKey: process.env.AWS_SECRET,
+    region: process.env.AWS_REGION
+})
+
+const uploadFileS3 = (fileName, fileData) => {
+    s3.upload({
+        Bucket: 'labb-photos',
+        Key: fileName,
+        Body: fileData
+    }, (error, data) => {
+        if (error) throw error;
+        // console.log(data.Location);
+    })
+}
 
 const authorize = function(req) {
     return req.session.user
@@ -30,6 +47,14 @@ exports.upload = function(req, res) {
 
     res.sendStatus(200);
 }
+
+exports.uploadS3 = function(req, res) {
+    const {image} = req.files;
+    if (!image) return res.sendStatus(400);
+    if (image.mimetype != 'image/png') return res.sendStatus(400);
+    //TODO: get image blob, uploadFileS3(filename, filedata)
+}
+
 
 exports.complete = function(req, res, next) {
     if (authorize(req)) {
