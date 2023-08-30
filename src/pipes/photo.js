e5c729c8eddfe2a9139a933072307b24cde6beec
MXP <jungsue.jungsue@gmail.com>
2023-08-30 21:25:13 +0900

    feat: implement photos page

--- src/pipes/photo.js
+++ src/pipes/photo.js
@@ -8,17 +8,25 @@ const s3 = new aws.S3({
     params: {Bucket: 'labb-photos'}
 });
 
-exports.listAlbums = () => {
+exports.listAlbums = (req, res, next) => {
     s3.listObjects({Delimiter: '/'}, function(err, data) {
       if (err) {
         throw new Error(err);
       } else {
-        // var albums = data.CommonPrefixes.map(function(commonPrefix) {
-        //   var prefix = commonPrefix.Prefix;
-        //   var albumName = decodeURIComponent(prefix.replace('/', ''));
-        //   return albumName;
-        // });
-        return data;
+        if (data && data.Contents) {
+          const MAX = 100;
+          let photos = data.Contents.slice(0, MAX);
+          photos.sort((a, b) => b.LastModified - a.LastModified);
+
+          res.render("photos", {
+            photos: photos
+          })
+        } else {
+          res.render("photos", {
+            photos: []
+          })
+        }
       }
     });
+
 }
